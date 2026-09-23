34da5ca6da765709981de14b74f0104ca95845a4
Vignesh Thilagaraj <vigneshthilagaraj00@gmail.com>
2026-09-23 21:48:10 +0530

    Example Navbar created successfully!

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
         <nav>
             <div className="nav-bar">
                 <div className="logo">
-                    <h1>vtx-Portfo</h1>
+                    <h1>VTX-22EP</h1>
                 </div>
                 <div className="nav-links">
                     <ul>
@@ -13,10 +13,13 @@ function Navbar() {
                         <li><a href="#">About</a></li>
                         <li><a href="#">Skills</a></li>
                         <li><a href="#">Project</a></li>
+                        <li><a href="#">Certificats</a></li>
+                        <li><a href="#">Learning</a></li>
                         <li><a href="#">Contact</a></li>
                     </ul>
                 </div>
             </div>
+            <div className="theme"></div>
         </nav>
     )
 }
